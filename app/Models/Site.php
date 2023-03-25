858b0ae7080f42ca037896ffe200b786d6cc2a9b
Caleb Oki <caleb.oki@ibm.com>
2023-03-25 17:33:12 -0400

    created site model and dashboard controller

---
+++ app/Models/Site.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Site extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'default',
+        'domain',
+        'scheme'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
