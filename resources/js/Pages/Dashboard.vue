6f45cf5e59e279fc21c2ea2ff3b9fdaa77184b88
Caleb Oki <caleb.oki@ibm.com>
2023-03-25 19:14:11 -0400

    fixed dashboardcontroller

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,10 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    site: Object
+})
 </script>
 
 <template>
@@ -14,7 +18,7 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div class="p-6 text-gray-900">{{ site }}</div>
                 </div>
             </div>
         </div>
