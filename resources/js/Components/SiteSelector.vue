594fa5437806eb190daf427b3d665783e77fff91
Caleb Oki <caleb.oki@ibm.com>
2023-04-08 13:37:36 -0400

    creating a site

--- resources/js/Components/SiteSelector.vue
+++ resources/js/Components/SiteSelector.vue
@@ -24,7 +24,7 @@
 
         <form v-on:submit.prevent="createSite" class="overflow-hidden space-y-4">
             <InputLabel for="domain" value="Domain" class="sr-only" />
-            <TextInput id="domain" type="text" class="block w-full h-9 text-sm" placeholder="e.g. https://codecourse.com" v-model="siteForm.domain" :class="{ 'border-red-500': siteForm.errors.domain }" />
+            <TextInput id="domain" type="text" class="block w-full h-9 text-sm" placeholder="e.g. https://uptime.com" v-model="siteForm.domain" :class="{ 'border-red-500': siteForm.errors.domain }" />
             <InputError class="mt-2" :message="siteForm.errors.domain" />
             <PrimaryButton>
                 Add
